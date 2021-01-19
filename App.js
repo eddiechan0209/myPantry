9430ad6b011cb004b279a1ca1a193319abc82c33
Eddie Chan <edcchan@ucsc.edu>
2021-01-19 13:06:29 -0800

    installed dependencies

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createSwitchNavigator} from 'react-navigation';
+
+import LoginScreen from './screens/LoginScreen';
+import DashboardScreen from './screens/DashboardScreen';
+import LoadingScreen from './screens/LoadingScreen';
+import { createAppContainer } from 'react-navigation';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>For myPantry!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen: LoadingScreen,
+  LoginScreen: LoginScreen,
+  DashboardScreen: DashboardScreen
+});
+
+const AppNavigator = createAppContainer(AppSwitchNavigator)
 
 const styles = StyleSheet.create({
   container: {
