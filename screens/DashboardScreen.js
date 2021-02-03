2ff169c7f4648947ae0f3f7f9c1bf99e43874919
Meelad Danai <meeladdanai@Meelads-MacBook-Pro.local>
2021-02-03 13:34:21 -0800

    Adding map to dashboard

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { 
     View,
     Text,
@@ -6,12 +6,76 @@ import {
     Button
 } from "react-native";
 import firebase from "firebase";
+import * as Permissions from 'expo-permissions'
+import * as Location from 'expo-location'
+import MapView from 'react-native-maps'
 
 class DashboardScreen extends Component {
+    
+    // MAP CODE TAKEN FROM https://snack.expo.io/@professorxii/expo-map-and-location-example
+    
+    state = {
+        mapRegion: null,
+        hasLocationPermissions: false,
+        locationResult: null
+      };
+    
+    componentDidMount = () => {
+        this.getLocationAsync();
+    }
+    
+    handleMapRegionChange = (mapRegion) =>{
+        console.log(mapRegion);
+        this.setState({ mapRegion });
+    }
+
+    async getLocationAsync (){
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+          this.setState({
+            locationResult: 'Permission to access location was denied',
+          });
+        } else {
+          this.setState({ hasLocationPermissions: true });
+        }
+    
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ locationResult: JSON.stringify(location) });
+    
+    // Center the map on the location we just fetched.
+        this.setState({mapRegion: { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }});
+    }
+
+    handleClickPantries = () => {
+        console.log(this.state.locationResult)
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text>DashboardScreen</Text>
+                <Text style={styles.paragraph}>
+                Pan, zoom, and tap on the map!
+                </Text>
+        
+                {
+                this.state.locationResult === null ?
+                <Text>Finding your current location...</Text> :
+                this.state.hasLocationPermissions === false ?
+                    <Text>Location permissions are not granted.</Text> :
+                    this.state.mapRegion === null ?
+                    <Text>Map region doesn't exist.</Text> :
+                    <MapView
+                    style={{ alignSelf: 'stretch', height: 400 }}
+                    region={this.state.mapRegion}
+                    onRegionChange={this.handleMapRegionChange}
+                    />
+                }
+        
+                <Text>
+                Location: {this.state.locationResult}
+                </Text>
+                <Button title="Find Closest Pantries" onPress={()=> this.handleClickPantries()}/>
                 <Button title="Sign out" onPress={() => firebase.auth().signOut()}/>
             </View>
         );
