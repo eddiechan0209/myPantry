4630696ba2a71a46b731190a6ab7f668fd9f1e8d
Meelad Danai <meeladdanai@Meelads-MacBook-Pro.local>
2021-02-23 03:01:51 -0800

    Adding functionality to the add inventory button

--- screens/InputPantryInfoScreen.js
+++ screens/InputPantryInfoScreen.js
@@ -5,12 +5,57 @@ import * as Permissions from 'expo-permissions';
 import * as Location from 'expo-location';
 import MapView from 'react-native-maps';
 
+const SERVER_URL = 'http://192.168.1.70:3000/pantries';
+
+const pantryEntry = {
+    method: 'POST',
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        name: 'Great Pantry', // Need to update to use current name 
+        address: 'Some address', // Need to update to use current address 
+        inventory: [],
+    }),
+};
+// Need to set inventory to empty array or push will not work
+pantryEntry.inventory = [];
+
 class InputPantryInfoScreen extends Component {
 
-    addValuesToPantry(){
-        // NEED TO ADD THE VALUES IN STATE TO THE PANTRY IF NOT EMPTY 
+    addValuesToPantry = async () => {
+        // Adding the values that were inputted to the existing inventory list
+        pantryEntry.inventory.push( {itemID: 1, name: this.state.itemName, quantity: this.state.itemQuantity}); 
+        console.log(pantryEntry);
+        console.log('id: ' + this.state.dbID);
+		console.log('in updateentry');
+        // Running update and then updating the values 
 
-    }
+            // Possible error to check for could be if Pantry is trying to add to existing item 
+            // like if the pantry is trying to add more apples and not replace total with the given
+            // number
+
+		return fetch(SERVER_URL + '/' + this.state.dbID, pantryEntry)
+			.then((response) => {
+				console.log('in first then');
+				console.log(response.status);
+				if (response.status >= 200 && response.status <= 299) {
+					return response.json();
+				} else {
+					console.log('error in UPDATE. statuscode: ' + response.status);
+				}
+			})
+			.then((responseJson) => {
+				console.log('in second then');
+				// console.log('reponse :' + JSON.stringify(responseJson));
+				console.log(responseJson);
+			})
+			.catch((error) => {
+				console.error(error);
+				console.error('^ is the error, in UPDATE');
+			});
+	};
 
     state = {
         dbID: '6033670df94993f43908e153',   
